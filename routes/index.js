28bde94c96b0acca3cea0a277f22764af3994c4b
Mary Margaret <mmlawton15@gmail.com>
2022-03-06 12:11:52 -0500

    finished getting the base code setup

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const htmlRoutes = require('./html-routes');
+//const htmlRoutes = require('./html-routes');
 
-router.use('/', htmlRoutes);
+//router.use('/', htmlRoutes);
 
 router.use((req, res) => {
     res.status(404).send('<h1> 404 Error! </h1>');
